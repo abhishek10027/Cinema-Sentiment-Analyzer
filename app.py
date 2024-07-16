7eca852f6ead4642e53e0ddda481c46d42d635ba
Abhishek Kushwaha <132592735+abhishek10027@users.noreply.github.com>
2024-07-16 22:48:52 +0530

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask, request, jsonify, send_from_directory
 import pickle
-import os
 
 app = Flask(__name__)
 
 # Load the CountVectorizer and model
-count_vectorizer_path = os.path.join(os.getcwd(), 'count_vectorizer.pkl')
-logistic_model_path = os.path.join(os.getcwd(), 'logistic_model.pkl')
-
-with open(count_vectorizer_path, 'rb') as f:
-    save_cv = pickle.load(f)
-
-with open(logistic_model_path, 'rb') as f:
-    model_s = pickle.load(f)
+save_cv = pickle.load(open('count_vectorizer.pkl', 'rb'))
+model_s = pickle.load(open('logistic_model.pkl', 'rb'))
 
 # Serve the static HTML file
 @app.route('/')
@@ -20,7 +13,7 @@ def home():
     return send_from_directory('.', 'index.html')
 
 # Define the route for processing sentiment analysis
-@app.route('/api/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     sentence = request.form['sentence']
     sen = save_cv.transform([sentence]).toarray()
